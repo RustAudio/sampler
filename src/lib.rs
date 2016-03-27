0b8ae1bc8d54a1815ec3560dff2790328d7ef068
mitchmindtree <mitchell.nordine@gmail.com>
2016-03-28 02:01:38 +1100

    Remove source and voice modules, add mode module. Expose Audio, Map, Sample, Sampler and Frames types and Mode trait. Add hound crate behind feature. Add instrument crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
+extern crate hound;
+extern crate instrument;
 extern crate pitch_calc as pitch;
 extern crate sample;
 extern crate time_calc as time;
 
+pub use map::{Audio, Map, Sample};
+pub use mode::Mode;
+pub use sampler::{Frames, Sampler};
+
 mod map;
+mod mode;
 mod sampler;
-mod source;
-mod voice;
 
 pub type Velocity = f32;
